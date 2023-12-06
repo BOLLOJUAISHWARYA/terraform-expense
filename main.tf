b2240c73405f225da5c5c0c473847aaf2ddb5f65
BOLLOJUAISHWARYA <bolloju.aishwarya@gmail.com>
2023-12-06 21:07:58 +0530

    expense-terraform

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "frontend" {
-  for_each =  var.components
-  ami = var.ami
-  instance_type = each.value
-  vpc_security_group_ids = var.sg_id
+resource "aws_instance" "instances" {
+  for_each                =  var.components
+  ami                     = var.ami
+  instance_type           = each.value["instance_type"]
+  vpc_security_group_ids  = var.sg_id
   tags = {
     Name = "${each.value["name"]}-${var.env}"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "instance" {
   for_each =  var.components
   zone_id = var.zone_id
   name    = "${each.value}-${var.env}"
