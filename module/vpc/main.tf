b67318ba289469b9af168dfbc534a01c5609fb12
BOLLOJUAISHWARYA <bolloju.aishwarya@gmail.com>
2023-12-07 17:53:36 +0530

    expense-terraform

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -3,3 +3,35 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, {Name = var.env})
 }
 
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  tags = merge(var.tags, {Name = "public-subnet"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "web" {
+  count = length(var.web_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  tags = merge(var.tags, {Name = "public-subnet"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "app" {
+  count = length(var.app_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  tags = merge(var.tags, {Name = "public-subnet"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  tags = merge(var.tags, {Name = "public-subnet"})
+  availability_zone = var.azs[count.index]
+}
+
